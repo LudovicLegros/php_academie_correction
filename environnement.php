19861c54a3acfb2a0e1a8c24831d92ae321e885b
Ludovic Legros <contact@ludoviclegros.fr>
2025-06-11 20:33:01 +0200

    Ajout de commentaires explicatif

--- environnement.php
+++ environnement.php
@@ -1,11 +1,14 @@
 <?php
 session_start();
 $bdd = new PDO('mysql:host=localhost;dbname=perigueux_academie;charset=utf8', 'root', '');
-//$_SERVER['SCRIPT_NAME'] donne dynamiquement le chemin d'accés jusqu'au fichier en cours
-//dirname récupère uniquement le chemin (sans le fichier index.php par exemple)
+//realpath(__DIR__) renvoie le chemin depuis la racine de la machine
+//$_SERVER['DOCUMENT_ROOT'] renvoie le chemin de la racine de la machine jusqu'a la racine du serveur PHP
+//str_replace change tout les \ en / pour avoir la même structure de chemin
+//On enleve ensuite le chemin d'accès racine > server PHP du chemin racine > projet 
 $projectRacine = str_replace('\\','/',realpath(__DIR__));
 $documentRoot = str_replace('\\','/',$_SERVER['DOCUMENT_ROOT']);
 $cheminRelatif = str_replace($documentRoot,'', $projectRacine);
-
+//la fonction define met dans le premier parametre le contenu du deuxieme parametre
 define('BASE_URL', $cheminRelatif);
-// var_dump($_SESSION);
+// var_dump($_SERVER);
+// var_dump($projectRacine);
